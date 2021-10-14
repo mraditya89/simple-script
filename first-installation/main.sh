f2db5fae72e1803f76a249c4aacbc3c9c1d6f55c
Aditya <42856317+mraditya89@users.noreply.github.com>
2021-10-14 14:35:56 +0700

    create main.sh

---
+++ first-installation/main.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+then
+  echo "[script] : Please run as root. Abort installation"
+  exit
+fi
+
+echo "[script] : Running the installation script"
+
+bash ./node-installation.sh
+bash ./nginx-installation.sh
+bash ./mongo-installation.sh
